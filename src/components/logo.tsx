c903892c979866955fd22625e2b1bd3de1d66227
SUJAN RATHOD <sujans2020@gmail.com>
2025-07-20 08:17:04 +0000

    use this image as logo please don't use any other logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -17,22 +17,55 @@ export const Logo: FC<LogoProps> = ({ className }) => {
     >
       <svg
         className="h-7 w-auto"
-        viewBox="0 0 110 32"
-        fill="currentColor"
+        viewBox="0 0 170 32"
+        fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M7 30V7.5H11V30H7Z" />
-        <path d="M1.5 8C1.5 7.72386 1.72386 7.5 2 7.5H16C16.2761 7.5 16.5 7.72386 16.5 8V8C16.5 12.1421 13.1421 15.5 9 15.5V15.5C4.85786 15.5 1.5 12.1421 1.5 8V8Z" stroke="currentColor" strokeWidth="3" />
-        <text
-          x="22"
-          y="24"
-          fontFamily="Inter, sans-serif"
-          fontSize="24"
-          fontWeight="bold"
-          className="font-headline"
-        >
-          ravonaa
-        </text>
+        <g clipPath="url(#clip0_1_2)">
+          <text 
+            x="0" 
+            y="24"
+            fontFamily="Inter, sans-serif"
+            fontSize="24"
+            fontWeight="bold"
+            fill="#212A4A"
+            className="font-headline"
+            letterSpacing="-0.5"
+          >
+            TRAV
+          </text>
+          
+          <g transform="translate(74, 5)">
+            <path 
+              d="M11.0859 0.707107C14.7869 -2.99393 21.2131 -2.99393 24.9141 0.707107L25.3354 1.12843C27.0583 2.85132 28 5.21406 28 7.66981V13.8284C28 19.8251 20.3553 25.5681 15.0006 25.5681C9.64593 25.5681 2 19.8251 2 13.8284V7.66981C2 5.21406 2.94172 2.85132 4.66462 1.12843L5.08594 0.707107C8.78693 -2.99393 15.2131 -2.99393 18.9141 0.707107"
+              transform="translate(-2, 0)" fill="#212A4A"
+            />
+            <path 
+              d="M13,0 A13,13 0 0,1 26,13 L13,13 Z"
+              fill="#343E47"
+            />
+            <circle cx="13" cy="13" r="1.5" fill="white"/>
+            <path d="M13 13 L21 5" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
+          </g>
+
+          <text
+            x="110"
+            y="24"
+            fontFamily="Inter, sans-serif"
+            fontSize="24"
+            fontWeight="bold"
+            fill="#343E47"
+            className="font-headline"
+            letterSpacing="-0.5"
+          >
+            NAA
+          </text>
+        </g>
+        <defs>
+          <clipPath id="clip0_1_2">
+            <rect width="170" height="32" fill="white"/>
+          </clipPath>
+        </defs>
       </svg>
     </Link>
   );
