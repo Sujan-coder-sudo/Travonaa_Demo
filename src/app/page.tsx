c11529e3978f1f7ea134681628f703f25e7391ca
SUJAN RATHOD <sujans2020@gmail.com>
2025-07-20 09:35:03 +0000

    buddy I said 100 times then to you are not understanding what I saying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,8 +103,8 @@ export default function Home() {
             <div className="relative mt-12">
                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full max-w-screen-xl h-2/3 bg-primary/5 rounded-t-full blur-3xl" />
                <Image
-                src="https://placehold.co/1200x600.png"
-                alt="Students on an industrial visit"
+                src="https://images.unsplash.com/photo-1623512396386-9a2bf9228537"
+                alt="Indian college students traveling in a bus for an industrial visit"
                 width={1200}
                 height={600}
                 className="relative mx-auto rounded-t-lg border object-cover shadow-2xl"
