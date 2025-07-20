49a396ac9dac54df07f2fb72238b074a016e46b9
SUJAN RATHOD <sujans2020@gmail.com>
2025-07-20 08:15:58 +0000

    this is not i want

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,18 +11,20 @@ export const Logo: FC<LogoProps> = ({ className }) => {
     <Link
       href="/"
       className={cn(
-        'flex items-center text-primary',
+        'flex items-center gap-2 text-primary',
         className
       )}
     >
       <svg
-        className="h-8 w-auto"
-        viewBox="0 0 180 32"
+        className="h-7 w-auto"
+        viewBox="0 0 110 32"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
       >
+        <path d="M7 30V7.5H11V30H7Z" />
+        <path d="M1.5 8C1.5 7.72386 1.72386 7.5 2 7.5H16C16.2761 7.5 16.5 7.72386 16.5 8V8C16.5 12.1421 13.1421 15.5 9 15.5V15.5C4.85786 15.5 1.5 12.1421 1.5 8V8Z" stroke="currentColor" strokeWidth="3" />
         <text
-          x="30"
+          x="22"
           y="24"
           fontFamily="Inter, sans-serif"
           fontSize="24"
@@ -31,9 +33,6 @@ export const Logo: FC<LogoProps> = ({ className }) => {
         >
           ravonaa
         </text>
-        <path
-          d="M19.16 3.2C17.56 2.4 15.76 2 13.6 2C11.44 2 9.64 2.4 8.04 3.2L8.2 5.2C9.64 4.4 11.44 4 13.6 4C15.76 4 17.56 4.4 18.92 5.2L19.16 3.2ZM13.6 30V4H15.6V30H13.6ZM11.6 30V4H13.6V30H11.6Z"
-        />
       </svg>
     </Link>
   );
